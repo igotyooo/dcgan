8265d6007f9a4d01748365248147081afc790628
igotyooo <overmars29@gmail.com>
2016-02-29 04:12:22 +0900

    Clean up variable names.

--- dtgan/pretrain_converter.py
+++ dtgan/pretrain_converter.py
@@ -28,7 +28,7 @@ nvis = 14         # # of samples to visualize during training.
 b1 = 0.5          # momentum term of adam.
 nz = 100 	  # # dim of central activation of converter.
 nc = 3            # # of channels in image.
-nbatch = 128      # # of examples in batch.
+batch_size = 128      # # of examples in batch.
 npx_s = 128       # # of pixels width/height of source images.
 npx_t = 64        # # of pixels width/height of target images.
 ncf = 128         # # of converter filters in last conv layer.
@@ -43,39 +43,39 @@ sigmoid = activations.Sigmoid(  )
 lrelu = activations.LeakyRectify(  )
 tanh = activations.Tanh(  )
 bce = T.nnet.binary_crossentropy
-tifn = inits.Normal( scale = 0.02 )
+filt_ifn = inits.Normal( scale = 0.02 )
 gain_ifn = inits.Normal( loc = 1., scale = 0.02 )
 bias_ifn = inits.Constant( c = 0. )
-cw0 = tifn( ( ncf / 2, nc, 5, 5 ), 'cw0' )
-cw1 = tifn( ( ncf, ncf / 2, 5, 5 ), 'cw1' )
+cw0 = filt_ifn( ( ncf / 2, nc, 5, 5 ), 'cw0' )
+cw1 = filt_ifn( ( ncf, ncf / 2, 5, 5 ), 'cw1' )
 cg1 = gain_ifn( ncf, 'cg1' )
 cb1 = bias_ifn( ncf, 'cb1' )
-cw2 = tifn( ( ncf * 2, ncf, 5, 5 ), 'cw2' )
+cw2 = filt_ifn( ( ncf * 2, ncf, 5, 5 ), 'cw2' )
 cg2 = gain_ifn( ( ncf * 2 ), 'cg2' )
 cb2 = bias_ifn( ( ncf * 2 ), 'cb2' )
-cw3 = tifn( ( ncf * 4, ncf * 2, 5, 5 ), 'cw3' )
+cw3 = filt_ifn( ( ncf * 4, ncf * 2, 5, 5 ), 'cw3' )
 cg3 = gain_ifn( ( ncf * 4 ), 'cg3' )
 cb3 = bias_ifn( ( ncf * 4 ), 'cb3' )
-cw4 = tifn( ( ncf * 8, ncf * 4, 5, 5 ), 'cw4' )
+cw4 = filt_ifn( ( ncf * 8, ncf * 4, 5, 5 ), 'cw4' )
 cg4 = gain_ifn( ( ncf * 8 ), 'cg4' )
 cb4 = bias_ifn( ( ncf * 8 ), 'cb4' )
-cw5 = tifn( ( nz, ncf * 8, 4, 4 ), 'cw5' )
+cw5 = filt_ifn( ( nz, ncf * 8, 4, 4 ), 'cw5' )
 cg5 = gain_ifn( nz, 'cg5' )
 cb5 = bias_ifn( nz, 'cb5' )
-cw5d = tifn( ( ncf * 8 * 4 * 4, nz, 1, 1 ), 'cw5d' )
+cw5d = filt_ifn( ( ncf * 8 * 4 * 4, nz, 1, 1 ), 'cw5d' )
 cg5d = gain_ifn( ncf * 8 * 4 * 4, 'cg5d' )
 cb5d = bias_ifn( ncf * 8 * 4 * 4, 'cb5d' )
-cw4d = tifn( ( ncf * 8, ncf * 4, 5, 5 ), 'cw4d' )
+cw4d = filt_ifn( ( ncf * 8, ncf * 4, 5, 5 ), 'cw4d' )
 cg4d = gain_ifn( ( ncf * 4 ), 'cg4d' )
 cb4d = bias_ifn( ( ncf * 4 ), 'cb4d' )
-cw3d = tifn( ( ncf * 4, ncf * 2, 5, 5 ), 'cw3d' )
+cw3d = filt_ifn( ( ncf * 4, ncf * 2, 5, 5 ), 'cw3d' )
 cg3d = gain_ifn( ( ncf * 2 ), 'cg3d' )
 cb3d = bias_ifn( ( ncf * 2 ), 'cb3d' )
-cw2d = tifn( ( ncf * 2, ncf, 5, 5 ), 'cw2d' )
+cw2d = filt_ifn( ( ncf * 2, ncf, 5, 5 ), 'cw2d' )
 cg2d = gain_ifn( ( ncf ), 'cg2d' )
 cb2d = bias_ifn( ( ncf ), 'cb2d' )
-cw1d = tifn( ( ncf, nc, 5, 5 ), 'cw1d' )
-convert_params = [ cw0, cw1, cg1, cb1, cw2, cg2, cb2, cw3, cg3, cb3, cw4, cg4, cb4, cw5, cg5, cb5,
+cw1d = filt_ifn( ( ncf, nc, 5, 5 ), 'cw1d' )
+converter_params = [ cw0, cw1, cg1, cb1, cw2, cg2, cb2, cw3, cg3, cb3, cw4, cg4, cb4, cw5, cg5, cb5,
         cw5d, cg5d, cb5d, cw4d, cg4d, cb4d, cw3d, cg3d, cb3d, cw2d, cg2d, cb2d, cw1d ]
 def converter( IS, w0, w1, g1, b1, w2, g2, b2, w3, g3, b3, w4, g4, b4, w5, g5, b5,
         w5d, g5d, b5d, w4d, g4d, b4d, w3d, g3d, b3d, w2d, g2d, b2d, w1d ):
@@ -97,11 +97,11 @@ def converter( IS, w0, w1, g1, b1, w2, g2, b2, w3, g3, b3, w4, g4, b4, w5, g5, b
 # DEFINE TRAINING FUNCTION: FORWARD(COSTS) AND BACKWARD(UPDATE).
 IS = T.tensor4(  )
 IT = T.tensor4(  )
-IT_hat = converter( IS, *convert_params )
+IT_hat = converter( IS, *converter_params )
 cost = T.mean( ( IT - IT_hat ) ** 2 )
 lrt = sharedX( lr )
 c_updater = updates.Adam( lr = lrt, b1 = b1, regularizer=updates.Regularizer( l2 = l2 ) )
-c_updates = c_updater( convert_params, cost )
+c_updates = c_updater( converter_params, cost )
 updates = c_updates
 
 # COMPILING TRAIN/TEST FUNCTIONS.
@@ -127,20 +127,20 @@ tset_val = di.d2set_val
 dataout = os.path.join( './dataout/', di.name.upper(  ) )
 desc = 'pretrain_converter'.upper(  )
 model_dir = os.path.join( dataout, desc, 'models'.upper(  ) )
-samples_dir = os.path.join( dataout, desc, 'samples'.upper(  ) )
-logs_dir = os.path.join( dataout, desc )
-if not os.path.exists( logs_dir ):
-    os.makedirs( logs_dir )
+sample_dir = os.path.join( dataout, desc, 'samples'.upper(  ) )
+log_dir = os.path.join( dataout, desc )
+if not os.path.exists( log_dir ):
+    os.makedirs( log_dir )
 if not os.path.exists( model_dir ):
     os.makedirs( model_dir )
-if not os.path.exists( samples_dir ):
-    os.makedirs( samples_dir )
-f_log = open( os.path.join( logs_dir, '%s.ndjson' % desc ), 'wb' )
+if not os.path.exists( sample_dir ):
+    os.makedirs( sample_dir )
+f_log = open( os.path.join( log_dir, '%s.ndjson' % desc ), 'wb' )
 log_fields = [
-    'n_epochs', 
-    'n_updates', 
-    'n_examples', 
-    'n_seconds',
+    'num_epoch', 
+    'num_update', 
+    'num_example', 
+    't_spent',
     'cost',]
 
 # PLOT SOURCE/TARGET SAMPLE IMAGES.
@@ -150,64 +150,64 @@ vis_tr = vis_tr[ 0 : nvis ** 2 ]
 vis_ims_tr_s = ims_tsize.take( sset_tr.take( vis_tr ), axis = 0 )
 vis_ims_tr_t = ims_tsize.take( tset_tr.take( vis_tr ), axis = 0 )
 color_grid_vis( vis_ims_tr_s, ( nvis, nvis ),
-            os.path.join( samples_dir, 'TR000S.png' ) )
+            os.path.join( sample_dir, 'TR000S.png' ) )
 color_grid_vis( vis_ims_tr_t, ( nvis, nvis ),
-            os.path.join( samples_dir, 'TR000T.png' ) )
+            os.path.join( sample_dir, 'TR000T.png' ) )
 vis_ims_tr_s = transform( ims_ssize.take( sset_tr.take( vis_tr ), axis = 0 ), npx_s )
 vis_val = np.random.permutation( len( sset_val ) )
 vis_val = vis_val[ 0 : nvis ** 2 ]
 vis_ims_val_s = ims_tsize.take( sset_val.take( vis_val ), axis = 0 )
 vis_ims_val_t = ims_tsize.take( tset_val.take( vis_val ), axis = 0 )
 color_grid_vis( vis_ims_val_s, ( nvis, nvis ),
-            os.path.join( samples_dir, 'VAL000S.png' ) )
+            os.path.join( sample_dir, 'VAL000S.png' ) )
 color_grid_vis( vis_ims_val_t, ( nvis, nvis ),
-            os.path.join( samples_dir, 'VAL000T.png' ) )
+            os.path.join( sample_dir, 'VAL000T.png' ) )
 vis_ims_val_s = transform( ims_ssize.take( sset_val.take( vis_val ), axis = 0 ), npx_s )
 
 # DO THE JOB.
 print desc.upper(  )
-n_updates = 0
+num_update = 0
 n_check = 0
-n_epochs = 0
-n_updates = 0
-n_examples = 0
+num_epoch = 0
+num_update = 0
+num_example = 0
 t = time(  )
 for epoch in range( niter ):
     # Load pre-trained param if exists.
-    n_epochs += 1
-    mpath = os.path.join( model_dir, '%03d.npy' % n_epochs )
+    num_epoch += 1
+    mpath = os.path.join( model_dir, '%03d.npy' % num_epoch )
     if os.path.exists( mpath ):
-        print( 'Epoch %02d: Load.' % n_epochs )
+        print( 'Epoch %02d: Load.' % num_epoch )
         data = np.load( mpath )
-        for pi in range( len( convert_params ) ):
-            convert_params[ pi ].set_value( data[ pi ] )
+        for pi in range( len( converter_params ) ):
+            converter_params[ pi ].set_value( data[ pi ] )
         continue
     # Training.
-    num_batches = int( np.ceil( sset_tr.shape[ 0 ] / float( nbatch ) ) )
+    num_batches = int( np.ceil( sset_tr.shape[ 0 ] / float( batch_size ) ) )
     for idx in range( num_batches ):
-        idxs = idx * nbatch
-        idxe = min( idx * nbatch + nbatch, sset_tr.shape[ 0 ] )
+        idxs = idx * batch_size
+        idxe = min( idx * batch_size + batch_size, sset_tr.shape[ 0 ] )
         ISb = transform( ims_ssize.take( sset_tr[ idxs : idxe ], axis = 0 ), npx_s )
         ITb = transform( ims_tsize.take( tset_tr[ idxs : idxe ], axis = 0 ), npx_t )
         cost = _train_c( ISb, ITb )
-        n_updates += 1
-        n_examples += len( ISb )
+        num_update += 1
+        num_example += len( ISb )
         if np.mod( idx, num_batches / 20 ) == 0:
             prog = np.round( idx * 100. / num_batches )
             print( 'Epoch %02d: %03d%% (batch %06d / %06d), cost = %.4f' 
-                    % ( n_epochs, prog, idx + 1, num_batches, float( cost ) ) )
+                    % ( num_epoch, prog, idx + 1, num_batches, float( cost ) ) )
     # Leave logs.
     c_cost = float( cost )
-    log = [ n_epochs, n_updates, n_examples, time(  ) - t, c_cost ]
+    log = [ num_epoch, num_update, num_example, time(  ) - t, c_cost ]
     f_log.write( json.dumps( dict( zip( log_fields, log ) ) ) + '\n' )
     f_log.flush(  )
     # Sample visualization.
     samples = np.asarray( _convert( vis_ims_tr_s ) )
     color_grid_vis( inverse_transform( samples, npx_t ), ( nvis, nvis ),
-            os.path.join( samples_dir, 'TR%03dT.png' % n_epochs ) )
+            os.path.join( sample_dir, 'TR%03dT.png' % num_epoch ) )
     samples = np.asarray( _convert( vis_ims_val_s ) )
     color_grid_vis( inverse_transform( samples, npx_t ), ( nvis, nvis ),
-            os.path.join( samples_dir, 'VAL%03dT.png' % n_epochs ) )
+            os.path.join( sample_dir, 'VAL%03dT.png' % num_epoch ) )
     # Save network.
-    print( 'Epoch %02d: Save.' % n_epochs )
-    np.save( mpath, [ p.get_value(  ) for p in convert_params ] )
+    print( 'Epoch %02d: Save.' % num_epoch )
+    np.save( mpath, [ p.get_value(  ) for p in converter_params ] )
